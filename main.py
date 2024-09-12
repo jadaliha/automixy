d2845fad1d2c2477293c6f4e3fb87cf534f25769
jadaliha <jadaliha@gmail.com>
2024-09-12 15:35:45 -0500

    🌟🚀dependancy removal works

--- main.py
+++ main.py
@@ -1,158 +1,120 @@
-from firedom.pretty import i
-from typing import Callable
+import weakref
+
+
+def print_debug(*args):
+    ...
+    # print('\033[91mDEBUG: \033[0m', *args )
 
 class binding:
-    def __init__(self, initial_value = None):
+    def __init__(self, initial_value=None):
         self._value = initial_value
-        self._observers = []
+        self._observers = set()  # Change to regular set
+        print_debug(f"Created binding with initial value {initial_value}")
 
     @property
     def value(self):
+        print_debug(f"Accessing binding value: {self._value}")
         return self._value
 
     def set(self, new_value):
+        print_debug(f"Attempting to set binding value from {self._value} to {new_value}")
         if self._value != new_value:
             self._value = new_value
-            for observer in self._observers:
-                observer()
+            print_debug(f"Value changed. Notifying {len(self._observers)} observers")
+            self._notify_observers()
+        else:
+            print_debug("Value unchanged, not notifying observers")
 
     def add_observer(self, observer):
-        self._observers.append(observer)
+        print_debug(f"Adding observer {observer} to binding")
+        self._observers.add(observer)
+        print_debug(f"Current observers: {list(self._observers)}")
 
-    def __repr__(self):
-        return f"binding({self._value})"
-
-    def __str__(self):
-        return str(self._value)
+    def _notify_observers(self):
+        print_debug(f"Notifying {len(self._observers)} observers")
+        for observer in list(self._observers):
+            print_debug(f"Notifying observer {observer}")
+            observer()._on_dependency_change()
 
 class reactive:
-    def __init__(self, func: Callable = lambda: None, *dependencies: binding, is_lazy: bool = True):
+    def __init__(self, func, *dependencies, is_lazy=True):
         self._func = func
         self._dependencies = list(dependencies)
         self._is_lazy = is_lazy
-        self._observers = []
+        self._observers = set()  # Change to regular set
         self._value = None
         self._is_dirty = True
+        print_debug(f"Created reactive with func {func}, dependencies {dependencies}, is_lazy={is_lazy}")
         self._setup_dependencies()
         if not is_lazy:
             self._update()
 
-    def __lshift__(self, args):
-        if isinstance(args, (tuple, set)):
-            self._func, *self._dependencies = args
-        else:
-            self._func = args
-            self._dependencies = []
-        self._is_lazy = False
-        self._setup_dependencies()
-        self._update()
-        return self
-
-    def __lt__(self, args):
-        if isinstance(args, (tuple, set)):
-            self._func, *self._dependencies = args
-        else:
-            self._func = args
-            self._dependencies = []
-        self._is_lazy = True
-        self._is_dirty = True
-        self._setup_dependencies()
-        return self
-
     def _setup_dependencies(self):
         for dep in self._dependencies:
-            if isinstance(dep, binding):
-                dep.add_observer(self._on_dependency_change)
-            elif isinstance(dep, reactive):
-                dep.add_observer(self._on_dependency_change)
+            if isinstance(dep, (binding, reactive)):
+                print_debug(f"Setting up dependency {dep}")
+                dep.add_observer(weakref.ref(self))
 
     def _on_dependency_change(self):
+        print_debug(f"Dependency changed for reactive {self}")
         if self._is_lazy:
             self._mark_dirty()
         else:
             self._update()
+        self._notify_observers()  # Add this line to propagate changes
 
     def _update(self):
+        print_debug(f"Updating reactive {self}")
         dep_values = [dep.value if isinstance(dep, (binding, reactive)) else dep for dep in self._dependencies]
-        self._value = self._func(*dep_values)
-        self._is_dirty = False
-        self._notify_observers()
+        new_value = self._func(*dep_values)
+        print_debug(f"Calculated new value: {new_value}")
+        if self._value != new_value:
+            self._value = new_value
+            self._is_dirty = False
+            print_debug(f"Value changed. Notifying {len(self._observers)} observers")
+            self._notify_observers()
+        else:
+            print("DEBUG: Value unchanged")
 
     def _mark_dirty(self):
         if not self._is_dirty:
+            print_debug(f"Marking reactive {self} as dirty")
             self._is_dirty = True
             self._notify_observers()
 
     def _notify_observers(self):
-        for observer in self._observers:
+        print_debug(f"Notifying {len(self._observers)} observers of reactive {self}")
+        for observer in list(self._observers):
+            print_debug(f"Notifying observer {observer}")
             observer()
 
     def add_observer(self, observer):
-        self._observers.append(observer)
+        print_debug(f"Adding observer {observer} to reactive {self}")
+        self._observers.add(observer)
+        print_debug(f"Current observers: {list(self._observers)}")
 
     def __call__(self):
+        print_debug(f"Calling reactive {self}")
         if self._is_dirty:
             self._update()
         return self._value
-    
-    def __repr__(self):
-        return f"reactive({self._func}, {self._dependencies})"
-    
+
     @property
     def value(self):
         return self.__call__()
 
-def PLUS_ONE(x):
-    print("PLUS_ONE called")
-    return x + 1  # Call x to get its value
-
-# Usage examples
-b = binding(5)
-
-# Eager evaluation
-# PLUS_ONE(b.value) will be evaluated immediately when b.value is set the result is cached and returned when a() is called
-a = reactive(PLUS_ONE, b, is_lazy=False)
-print("Eager evaluation result:", a())  # Should print 6 without re-evaluating func because the value is calculated once we execute a << {func, b} and when we set b the value is recalculated and cached eagerly
-
-
-# New usage example with lazy binding using <
-c = reactive(PLUS_ONE, b, is_lazy=True)
-
-# the PLUS_ONE(b.value) is not calculated till you call c(), the value is cashed until we set b again, when you need the value of c:
-result = c()
-print("Lazy binding result:", result)  # Should print 6
-
-# Demonstrate that changing b's value affects both a and c
-print("Updating b.value to 10")
-b.set(10)
-print("Updated eager evaluation result (a):", a.value)  # Should print 11
-print("Updated lazy binding result (c):", c())  # should call PLUS_ONE(b.value) and print 11 
-
-def SUM_TRIPLE(x, y, z):
-    print("SUM_TRIPLE called")
-    return x + y + z
-
-B = binding(2)
-A = reactive(SUM_TRIPLE, b, B, 3)
-A()  # This will evaluate SUM_TRIPLE immediately
-
-print("Value of A depends on B, and b:", A())
-
-B.set(10)
-b.set(20)
-print("Value of A depends on B, and b and should be 33:", A())
-
-
-
-def SUM_TUPLE(x, y):
-    print("SUM_TUPLE called")
-    return x+y
-R = reactive(SUM_TUPLE, a, B)
-print("Value of R depends on b and B, because a depends on b:", R())
-
-
-# Ultimate Test
-# reactive evaluation based on another reactive
-# here a is a reactive function of b, and P is a reactive function of a and B, and b, therefore P is a reactive function of b and B
-P = reactive(SUM_TRIPLE, a, B, b, is_lazy=False)
-P()
+    def __del__(self):
+        print_debug(f"Cleaning up reactive {self}")
+        weakref.getweakrefs(self)
+        for dep in self._dependencies:
+            if isinstance(dep, (binding, reactive)):
+                dependancies = set()
+                while dep._observers:
+                    o = dep._observers.pop()
+                    if o() == self:
+                        break
+                    else:
+                        dependancies.add(o)
+                while dependancies:
+                    o = dependancies.pop()
+                    dep._observers.add(o)
